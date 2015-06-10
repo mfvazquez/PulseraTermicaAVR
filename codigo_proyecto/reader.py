f5b5fe22537f017a095d518b0737fbb1b4ab9ef2
Xero-Hige <gaston.martinez.90@gmail.com>
2015-06-09 23:05:06 -0300

    Por las dudas que este defasado

--- codigo_proyecto/reader.py
+++ codigo_proyecto/reader.py
@@ -10,7 +10,10 @@ def main():
 	serial_port = serial.Serial("/dev/ttyUSB0",4800)
 	read_byte = serial_port.read(1)	
 	while (read_byte): #Leo el tipo de dato a leer
-		data_type,data_len = DATA_TYPE[read_byte]
+		try:
+			data_type,data_len = DATA_TYPE[read_byte]
+		except:
+			continue		
 		print "Leyendo: ",data_type
 		read_data = serial_port.read(data_len)
 		if not read_data:
