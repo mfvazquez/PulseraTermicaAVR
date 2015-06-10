904e4156bab4f0a67c2a7a99015f15c5f66fa417
Xero-Hige <gaston.martinez.90@gmail.com>
2015-06-09 23:01:57 -0300

    Agrego script para leer y algo al codigo para que mande

---
+++ codigo_proyecto/reader.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+import serial
+
+TAMANIO_TEMPERATURA = 1
+
+DATA_TYPE = {"A":("Temperatura Ambiente",TAMANIO_TEMPERATURA),"P":("Temperatura Peltier",TAMANIO_TEMPERATURA),
+		"I":("Numero Ciclo",1)}
+
+def main():
+	serial_port = serial.Serial("/dev/ttyUSB0",4800)
+	read_byte = serial_port.read(1)	
+	while (read_byte): #Leo el tipo de dato a leer
+		data_type,data_len = DATA_TYPE[read_byte]
+		print "Leyendo: ",data_type
+		read_data = serial_port.read(data_len)
+		if not read_data:
+			print "Fallo lectura"
+			break
+		print data_type, " : " , read_data
+
+main()
