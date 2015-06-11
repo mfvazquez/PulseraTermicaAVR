0466364f803a349ae6ce8776534f8a565bb94518
Xero-Hige <Gaston.Martinez.90@gmail.com>
2015-06-10 21:35:02 -0300

    Actualizo las cosas de la clase

--- codigo_proyecto/reader.py
+++ codigo_proyecto/reader.py
@@ -3,22 +3,28 @@ import serial
 
 TAMANIO_TEMPERATURA = 1
 
-DATA_TYPE = {"A":("Temperatura Ambiente",TAMANIO_TEMPERATURA),"P":("Temperatura Peltier",TAMANIO_TEMPERATURA),
-		"I":("Numero Ciclo",1)}
+VOLT = 2.56 / 255
+
+def traducir_voltaje(leido):
+	return ord(leido) * VOLT
+
+DATA_TYPE = {"A":("Temperatura Ambiente",TAMANIO_TEMPERATURA,traducir_voltaje),"P":("Temperatura Peltier",TAMANIO_TEMPERATURA,traducir_voltaje),
+		"I":("Numero Ciclo",1,ord)}
 
 def main():
 	serial_port = serial.Serial("/dev/ttyUSB0",4800)
-	read_byte = serial_port.read(1)	
-	while (read_byte): #Leo el tipo de dato a leer
-		try:
-			data_type,data_len = DATA_TYPE[read_byte]
-		except:
-			continue		
-		print "Leyendo: ",data_type
+	while (True): #Leo el tipo de dato a leer
+		read_byte = serial_port.read(1)	
+		if not DATA_TYPE.has_key(read_byte):
+			print "Leido: ",ord(read_byte)
+			continue
+
+		data_type,data_len,f = DATA_TYPE[read_byte]
 		read_data = serial_port.read(data_len)
 		if not read_data:
 			print "Fallo lectura"
 			break
-		print data_type, " : " , read_data
 
+		print data_type, " : " , f(read_data)
+		
 main()
