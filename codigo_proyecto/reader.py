ec5feb924cc65debdf1d3fcd297403a88a3e9a56
Matias Vazquez <matias@odin>
2015-06-14 20:23:14 -0300

    agregue el timmer y algunas cosas mas

--- codigo_proyecto/reader.py
+++ codigo_proyecto/reader.py
@@ -1,15 +1,26 @@
 #! /usr/bin/python
 import serial
+from time import time
 
 TAMANIO_TEMPERATURA = 1
 
 VOLT = 2.56 / 255
+A = -33.728
+B = 65.913
+
 
 def traducir_voltaje(leido):
 	return ord(leido) * VOLT
 
+def traducir_voltaje_temperatura(leido):
+	return traducir_voltaje(leido)*A+B
+
+def devolver_tiempo(leido):
+	print time()
+	return ord(leido)
+
 DATA_TYPE = {"A":("Temperatura Ambiente",TAMANIO_TEMPERATURA,traducir_voltaje),"P":("Tension Peltier",TAMANIO_TEMPERATURA,traducir_voltaje),
-		"I":("Numero Ciclo",1,ord)}
+		"I":("\nNumero Ciclo",1,devolver_tiempo), "T":("Temperatura Peltier",TAMANIO_TEMPERATURA,traducir_voltaje)}
 
 def main():
 	serial_port = serial.Serial("/dev/ttyUSB0",4800)
